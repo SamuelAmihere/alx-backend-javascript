77628a912f4a491b10fbaa0810502e8ee573baf5
Samuel Amihere <amisam2000@gmail.com>
2024-04-09 13:53:16 +0800

    Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,10 @@
-export default function createIteratorObject (report) {
+export default function createIteratorObject(report) {
   const departments = Object.keys(report.allEmployees);
-  return (function * () {
-    for (let i = 0; i < departments.length; i++) {
-      const employees = report.allEmployees[departments[i]];
-      for (let j = 0; j < employees.length; j++) {
-        yield employees[j];
+  return (function  p() {
+    for (const department of departments) {
+      const employees = report.allEmployees[department];
+      for (const emp of employees) {
+        yield emp;
       }
     }
   }());
