b764742389c665d4bb22837f42d99c33032703a9
Samuel Amihere <amisam2000@gmail.com>
2024-04-09 14:10:17 +0800

    Iterating through report objects

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-export default function appendToEachArrayValue (array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   const myArr = [];
   for (const i of array) {
     myArr.push(`${appendString}${i}`);
