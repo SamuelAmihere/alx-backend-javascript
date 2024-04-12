d88a93e85cc51b0e59f5b8e96e9589f5057342b6
Samuel Amihere <amisam2000@gmail.com>
2024-04-12 10:02:14 +0800

    ES6 Classes: A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -4,7 +4,7 @@ export default class Building {
       throw TypeError('sqft must be a number');
     }
     this._sqft = sqft;
-    if (this.constructor === Building) {
+    if (this.constructor !== Building) {
       if (this.evacuationWarningMessage !== 'function') {
         throw Error('Class extending Building must override evacuationWarningMessage');
       }
@@ -13,12 +13,5 @@ export default class Building {
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw TypeError('sqft must be a number');
-    }
-    this._sqft = value;
   }
 }
