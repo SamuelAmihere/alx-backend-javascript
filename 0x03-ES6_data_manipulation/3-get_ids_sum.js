59af822c754dca342350aa8561580ea2c7309f81
Samuel Amihere <amisam2000@gmail.com>
2024-04-17 11:25:10 +0800

    Object manipulation: Reduce

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,5 +2,5 @@ export default function getStudentIdsSum(students) {
   if (!Array.isArray(students)) {
     return 0;
   }
-  return students.reduce((acc, student) => acc + student.id, 0);
+  return students.reduce((accum, student) => accum + student.id, 0);
 }
