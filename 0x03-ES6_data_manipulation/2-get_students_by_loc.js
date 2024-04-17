f48fd5e7aa8ccb8d2e0907c8506eca98aac5bce2
Samuel Amihere <amisam2000@gmail.com>
2024-04-17 11:09:58 +0800

    Object manipulation: Filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,9 @@
 export default function getStudentsByLocation(students, city) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  if (typeof city !== 'string') {
+    return [];
+  }
   return students.filter((student) => student.location === city);
 }
