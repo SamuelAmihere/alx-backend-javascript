d48205e6af95aed1dbc3dea6abee84825b634059
Samuel Amihere <amisam2000@gmail.com>
2024-04-17 11:58:01 +0800

    Object manipulate: Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length || length <= 0) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  if (position >= view.length) {
+    throw new Error('Position outside range');
+  }
+  view[position] = value;
+  return buffer;
+}
