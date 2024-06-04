dcfd5da2fba0836abaee5681b516232b631eb8bf
Samuel Amihere <amisam2000@gmail.com>
2024-06-05 07:20:33 +0800

    Node_JS_basic: user input

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+/**
+ * Write a script that reads and waits for user input using
+ * stdin.
+ */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
+  console.log(`Your name is: ${name}`);
+  rl.close();
+});
+
