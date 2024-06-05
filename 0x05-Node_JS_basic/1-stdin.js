c11d5c5089f5c48c90681b36d7d2190f6338bb95
Samuel Amihere <amisam2000@gmail.com>
2024-06-05 08:02:59 +0800

    Node_JS_basic: user input

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,9 @@
-/**
- * Write a script that reads and waits for user input using
- * stdin.
- */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
   if (name) {
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+    process.stdout.write(`Your name is: ${name}`);
   } else {
     process.stdout.write('You need to provide a name\n');
   }
@@ -16,4 +11,3 @@ process.stdin.on('data', (data) => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
