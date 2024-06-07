fe7d05e632e5a1fc4f887efe0be527245aa07392
Samuel Amihere <amisam2000@gmail.com>
2024-06-07 13:15:02 +0800

    Test

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,19 +4,20 @@ const calculateNumber = require('./1-calcul');
 describe('calculateNumber', () => {
   describe('SUM', () => {
     it('should return the sum of the rounded numbers', () => {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1.5, 3), 5);
-        assert.strictEqual(calculateNumber(1.3, 3.1), 4);
-        assert.strictEqual(calculateNumber(-1, 1), 0);
-        assert.strictEqual(calculateNumber(-1.5, 1), 0);
-        assert.strictEqual(calculateNumber(-1.4, 1.4), 0);
-        assert.strictEqual(calculateNumber(0.0, 0), 0);    });
+        assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+        assert.strictEqual(calculateNumber('SUM', 1.5, 3), 5);
+        assert.strictEqual(calculateNumber('SUM', 1.3, 3.1), 4);
+        assert.strictEqual(calculateNumber('SUM', -1, 1), 0);
+        assert.strictEqual(calculateNumber('SUM', -1.5, 1), 0);
+        assert.strictEqual(calculateNumber('SUM', -1.4, 1.4), 0);
+        assert.strictEqual(calculateNumber('SUM', 0.0, 0), 0);
+    });
   });
 
   describe('SUBTRACT', () => {
     it('should return the subtraction of the rounded numbers', () => {
-       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -3);
-       assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 1.4), 3);
+       assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+       assert.strictEqual(calculateNumber('SUBTRACT', 4.5, 1.4), 4);
        assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 1.4), 0);
        assert.strictEqual(calculateNumber('SUBTRACT', 0.0, 0), 0);
        assert.strictEqual(calculateNumber('SUBTRACT', -2.3, -1.8), 0);
@@ -26,7 +27,7 @@ describe('calculateNumber', () => {
   describe('DIVIDE', () => {
     it('should return the division of the rounded numbers', () => {
        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
-       assert.strictEqual(calculateNumber('DIVIDE', 4.5, 1.4), 3.25);
+       assert.strictEqual(calculateNumber('DIVIDE', 4.5, 1.4), 5);
        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 1.4), 1);
        assert.strictEqual(calculateNumber('DIVIDE', 0.0, 0), 'Error');});
        assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
